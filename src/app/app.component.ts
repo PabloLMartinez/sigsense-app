1c8b3f9dc4cd0c496380942329c8313ac156762a
PabloLMartinez <pablo.martinez152@gmail.com>
2019-07-13 17:31:17 -0300

    Adding Bootstrap 4 and removing useless code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sigsenseApp';
-}
+export class AppComponent {}
